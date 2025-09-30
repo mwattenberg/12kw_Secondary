1c78e7eaadda7d1d42fea835adcd80391eced504
Martin Wattenberg <martin.wattenberg@infineon.com>
2025-09-30 09:19:40 +0200

    Added more code to the state machine

--- System.c
+++ System.c
@@ -8,10 +8,14 @@
 #include "ADC.h"
 #include "SPI.h"
 #include "DataStream.h"
+#include "cy_gpio.h"
+#include "System.h"
+#include "PWM.h"
 
 DataStream_data_buffer_t buffer;
 
 int8_t counter = 0;
+SystemState_t state;
 
 static void inline updatePowerScope()
 {
@@ -34,10 +38,9 @@ static void inline updatePowerScope()
 		DataStream_WriteBufferToOutput(&buffer);
 }
 
-void System_UpdateStateMachine()
+
+void doFanControl()
 {
-	if(Cy_SCB_UART_IsTxComplete(UART_PowerScope_HW))
-		updatePowerScope();
 	
 }
 
@@ -53,6 +56,73 @@ void System_init()
 	DataStream_init_buffer(&buffer);
 	
 }
+
+void System_UpdateStateMachine()
+{
+	static uint32_t myCounter = 0;
+	
+	if(Cy_SCB_UART_IsTxComplete(UART_PowerScope_HW))
+		updatePowerScope();
+	
+	switch (state)
+	{
+		case STATE_INIT:
+		myCounter = 0;
+		
+		Cy_GPIO_Set(PIN_LED_FAULT_PORT, PIN_LED_FAULT_PIN);
+		Cy_GPIO_Set(PIN_DISABLE_FAN_PORT, PIN_DISABLE_FAN_PIN);
+		
+		Cy_GPIO_Set(PIN_DIS_OUTPUT_PORT, PIN_DIS_OUTPUT_PIN);
+		Cy_GPIO_Clr(PIN_EN_OUTPUT_PORT, PIN_EN_OUTPUT_PIN);
+		
+			break;
+		case STATE_CHECK_FAN:
+		if(myCounter < 50)
+			PWM_setFanDuty(0.25f);
+		else if (myCounter < 100)
+ 			PWM_setFanDuty(0.0f);
+		else
+		{
+			state = STATE_CHECK_VOUT;
+			myCounter = 0;
+		}
+			
+			
+			break;
+		case STATE_CHECK_VOUT:
+			break;
+		case STATE_ENABLE_LINEAR_FET:
+			break;
+		case STATE_ENABLE_ORING:
+		Cy_GPIO_Clr(PIN_DIS_OUTPUT_PORT, PIN_DIS_OUTPUT_PIN);
+		Cy_GPIO_Set(PIN_EN_OUTPUT_PORT, PIN_EN_OUTPUT_PIN);
+			break;
+		case STATE_RUN:
+		Cy_GPIO_Clr(PIN_LED_FAULT_PORT, PIN_LED_FAULT_PIN);
+		Cy_GPIO_Set(PIN_LED_RUN_PORT, PIN_LED_RUN_PIN);
+		doFanControl();
+			break;
+			
+		case STATE_AC_LOSS:
+			Cy_GPIO_Set(PIN_AC_LOSS_PORT, PIN_AC_LOSS_PIN);
+			break;
+		
+		case STATE_OTP:
+//			break;	//No break, handle all error in same state
+		case STATE_OCP:
+//			break;	//No break, handle all error in same state
+		case STATE_ERROR:
+		Cy_GPIO_Set(PIN_LED_FAULT_PORT, PIN_LED_FAULT_PIN);
+		
+		Cy_GPIO_Set(PIN_DIS_OUTPUT_PORT, PIN_DIS_OUTPUT_PIN);
+		Cy_GPIO_Clr(PIN_EN_OUTPUT_PORT, PIN_EN_OUTPUT_PIN);
+			break;
+		
+	}
+
+}
+	
+
 
 
 
--- System.h
+++ System.h
@@ -10,6 +10,23 @@
 
 #include "cybsp.h"
 
+
+typedef enum {
+	STATE_INIT = 0,
+	STATE_CHECK_VOUT = 1,
+	STATE_CHECK_FAN = 2,
+	STATE_ENABLE_LINEAR_FET = 4,
+	STATE_ENABLE_ORING = 5,
+	STATE_RUN = 10,
+		
+	//Error states
+	STATE_AC_LOSS = 97,
+	STATE_OTP = 98,
+	STATE_OCP = 99,
+	STATE_ERROR = 100
+} SystemState_t;
+
+
 void System_init();
 void System_UpdateStateMachine();
 
